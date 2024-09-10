cdc2089bb8015b937eafb7284b6b4c90f743b394
Gail <11055380@uvu.edu>
2024-09-10 17:28:28 -0600

    Ping pong script

--- GameObjectsAndTransformLab/Assets/Scripts/Code.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/Code.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    public GameObject targetObject;
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
+        //Move the target GameObject
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+        //Rotate the target GameObject
+        transform.Rotate(new Vector3(0, 3, 0) * Time.deltaTime);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Access the Transform component of the target object
-        Transform targetTransform = targetObject.transform;
-
-        // Modify position
-        targetTransform.position = new Vector3(1f, 2f, 3f); // Set a new position
-
-        // Modify rotation
-        targetTransform.rotation = Quaternion.Euler(45f, 30f, 0f); // Set a new rotation
-
-        // Modify scale
-        targetTransform.localScale = new Vector3(2f, 1f, 0.5f); // Set a new scale
     }
 }
